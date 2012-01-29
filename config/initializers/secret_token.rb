d3e0be701841e6d8a0995e85af9ac1fba379f8df
Nicholas Yianilos <spartandeveloper@gmail.com>
2012-01-29 13:17:42 -0500

    Generated the application skeleton with updated .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partygap::Application.config.secret_token = '37ed1182b23cb6114156abfceba53aac5e745a967e39446769106aa726eb941c227f249cca1285cf903980e49713ac87ce7635f9e126f7826fdbf71f0bbd4ce4'
